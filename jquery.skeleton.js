b529960c191b746122df3941cacacf3acc305b28
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2013-05-29 16:16:54 +0200

    edits
    misschien helpt dit :D

--- jquery.skeleton.js
+++ jquery.skeleton.js
@@ -1,48 +1,58 @@
 ;(function($, window, document, undefined){
-	var defaults = {
-		label: 'yeahhhhh',
-		color: 'green'
-	};
-
-	var optionss = {
-		"username": "mauz0r",
-
-		"init": function (options) {
-			options = $.extend({}, defaults, options);
-			// Do whatever
-			console.log(optionss.getUsername());
-			optionss.setUsername("mau");
-
-			console.log('init');
-			this.css('color', options.color);
-		},
-		"bar": function (options) {
-			// Do whatever
-			console.log('boem!');
-
-			console.log(optionss.getUsername());
-
-			options = $.extend({}, defaults, options);
-			this.css('color', options.color).text(this.text() + ' ' + options.label);
-		},
-		"getUsername": function() {
-			return this.username;
-		}, 
-		"setUsername": function(name) {
-			this.username = name;
-		}
-	};
 
 	$.fn.skeleton = function(options) { 
+
+		var defaults = {
+			label: 'yeahhhhh',
+			color: 'green',
+			changeColor: 'orange'
+		};
+
+		var methods = {
+			"username": "mauz0",
+			"backupColor": false,
+
+			"init": function (options) {
+				options = $.extend({}, defaults, options);
+				// Do whatever
+				console.log(methods.getUsername());
+				methods.setUsername("mau");
+
+				console.log('init');
+				this.css('color', options.color);
+			},
+			"bar": function (options) {
+				// Do whatever
+				console.log('boem!');
+
+				console.log(methods.getUsername());
+
+				options = $.extend({}, defaults, options);
+				this.css('color', options.color).text(this.text() + ' ' + options.label);
+			},
+			"getUsername": function() {
+				return this.username;
+			}, 
+			"setUsername": function(name) {
+				this.username = name;
+			}, 
+			"changeIt": function(params) {
+				var localOptions = $.extend({}, defaults, params);
+				console.log(this);
+				this.css('color', localOptions.changeColor);
+			}
+		};
+
+
 		var args = arguments;
 		var argss = Array.prototype.slice.call(args, 1);
 
 		return this.each(function() {
 			var $this = $(this);  // Might make sense to ignore this and just pass `this` to the following things
-			if (optionss[options]) {
-				optionss[options].apply($this, argss);
+			if (methods[options]) {
+				methods[options].apply($this, argss);
 			} else if (typeof options === "object" || !options) {
-				optionss.init.apply($this, args);
+				methods.init.apply($this, args);
 			} else {
 				$.error("options " + options + " does not exist on jQuery.pollServer");
 			}
